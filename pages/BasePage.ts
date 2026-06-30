fab3cbf5d7a9f86c7714a5159ef031186b3c7ca5
pedrobarragan30 <pedrobarragan30@gmail.com>
2026-06-29 23:57:46 -0600

    pages/BasePage.ts  - Updates performed

--- pages/BasePage.ts
+++ pages/BasePage.ts
@@ -1,10 +1,11 @@
-import { Page, Locator } from "@playwright/test"; //"Page" and "Locator" needs to be used for POM
+import type { Page, Locator } from "@playwright/test"; //"Page" and "Locator" needs to be used for POM
 
 export class BasePage {
   protected chain: Promise<unknown> = Promise.resolve();
 
   //PB TEST: The Class constructor always exist, it's implicit; however, sometimes it needs to be defined explicitly.
   constructor(protected readonly page: Page, seedChain?:Promise<unknown>){};
+  //constructor(protected readonly page: Page) {}
 
   protected tid(base: string): Locator {
     //PB TEST: The values inside of the parenthesis is called "Signature".
@@ -19,11 +20,17 @@ export class BasePage {
       await this.page.waitForURL(pattern, { timeout });
     })    
   }
+  // protected async waitForUrl(pattern: RegExp, timeout = 15_000): Promise<void> {
+  //   await this.page.waitForURL(pattern, { timeout });
+  // }
 
   //PB TEST: IF Protected or Private it's not defined then it's treated as Public
   screenshot(name: string): this {
     return this.step(() => this.page.screenshot({ path: `test-results/${name}.png` }))
   }
+  // async screenshot(name: string): Promise<void> {
+  //   await this.page.screenshot({ path: `test-results/${name}.png` });
+  // }
 
   //Don't Repeat Yourself - DRY
   protected typeInput(locator: string, text: string): this {
@@ -32,15 +39,19 @@ export class BasePage {
     await this.tid(locator).fill(text);
     })
   }
+  // protected async typeInput(locator: string, text: string): Promise<void> {
+  //   await this.tid(locator).clear();
+  //   await this.tid(locator).fill(text);
+  // }
   
-  //PB NOTE: <unknown> indicate that this is an "unknown" promise where we do not know waht will be returned
+  //PB NOTE: <unknown> indicate that this is an "unknown" promise where we do not know what will be returned
   protected step(action: ()=> Promise <unknown>): this{
     this.chain = this.chain.then(() => action());
     return this;
   }
   
-  //PB NOTE: The next one is a "generic", thsoe are methods that have no idea what will come in and neither what will come out
-  //TResult returns nothigng. while TResult2 probably would be never called
+  //PB NOTE: The next one is a "generic", those are methods that have no idea what will come in and neither what will come out
+  //TResult returns nothing. while TResult2 probably would be never called
   then<TResult1 = void, TResult2 = never>(
     onfulfilled?: ((value:void) => TResult1 | PromiseLike<TResult1>) | null, 
     onrejected?: ((reason:unknown) => TResult2 |PromiseLike<TResult2>) | null,
