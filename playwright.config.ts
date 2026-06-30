d5af2264a10bdd7672264ae8aaf82ca874643ddc
pedrobarragan30 <pedrobarragan30@gmail.com>
2026-06-29 23:50:24 -0600

    playwright.config.ts  - Updates performeds

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,34 +9,34 @@ import dotenv from 'dotenv';
 import path from 'path';
 dotenv.config({ path: path.resolve(__dirname, '.env') });*/
 import "dotenv/config";
+const STORAGE_STATE = ".auth/user.json";
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
   //testDir: './tests', //PB NOTE: ORIGINAL LINE
-  testDir: ".", 
+  testDir: ".",
     //PB NOTE: UPDATED: This line specifies the directory where Playwright should look for test files. By setting it to ".", you are
     // telling Playwright to look for test files in the current directory and its subdirectories. This allows you to organize your test
     // files in a way that suits your project structure, without being limited to a specific "tests" directory.
-  testMatch: [/module-.*\/.*\.spec\.ts/, /tests\/.*\.spec\.ts/],
+  testMatch: [/module-.*\/.*\.(spec|setup)\.ts/, /tests\/.*\.spec\.ts/],
     //PB NOTE: UPDATED: This line specifies the pattern for test files that Playwright should look for. In this case, the pattern looks
     // for files that end with "spec.ts" and are located in either a "module-*" directory or a "tests" directory.
   /* Run tests in files in parallel */
-  fullyParallel: true, 
+  fullyParallel: true,
     //PB NOTE: TRUE for all browsers, but you can set this to false to run them sequentially.
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */ 
+  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
     //PB NOTE: If it fails once, Playwright will retry as many as many times as specified, If CI is set 2 retries will be performed. If
     // not, no retries will be performed.
-  /* Opt out of parallel tests on CI. */ 
+  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined, //PB NOTE: How the work in parallel each browser is a worker.
 
-
   timeout: 60_000,
-    //PB NOTE: ADDED: This will help to allow the server to coldstart and load the page before the test times out.  By configuring an 
+    //PB NOTE: ADDED: This will help to allow the server to cold start and load the page before the test times out.  By configuring an 
     // appropriate timeout, you can improve the reliability of your tests and ensure that they have enough time to complete successfully
     // under various conditions.
   expect: {timeout: 10_000},
@@ -50,9 +50,9 @@ export default defineConfig({
     // readability, especially when dealing with large numbers, but it does not change the underlying value or behavior of the code.
 
 
-  /* Re orter to use. See https://playwright.dev/docs/test-reporters */
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   //reporter: 'html', //PB NOTE: There can be reports in html, Junit, xml
-  reporter:[["html",{open: "always"}], ["list"]],
+  reporter: [["html", {open: "always"}], ["list"]],
     //PB NOTE: Available values - "always", "never", "on-failure", "on-first-retry", "on-all-retries". This line configures the HTML
     // reporter to generate a report but not automatically open it after the tests are completed. The "open: 'never'" option ensures that
     // the report is generated and saved to disk, but it will not be opened in a browser automatically. This allows you to review the
@@ -71,7 +71,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
       //PB NOTE: ORIGINAL LINE
-    baseURL: process.env.BASE_URL ?? "https://omnipizza-frontend.onrender.com", 
+    baseURL: process.env.BASE_URL ?? "https://omnipizza-frontend.onrender.com",
       //PB NOTE: ADDED: The "??" Indicates that If the BASE_URL environment variable is not set, it will default to the specified
       // URL.
 
@@ -84,16 +84,30 @@ export default defineConfig({
       //PB NOTE: ADDED: Available values are "on", "off", "on-first-retry", "retain-on-failure", "retry-with-video". This line configures Playwright to record videos of test executions only when a test fails.
     navigationTimeout: 45_000,
       //PB NOTE: ADDED: This line sets the maximum time (in milliseconds) that Playwright will wait for a page navigation to complete before timing out. If a page takes longer than this specified time to load or navigate, Playwright will consider it a failure and throw a timeout error.
-    actionTimeout: 15_000, 
+    actionTimeout: 15_000,
       //PB NOTE: ADDED:  This line sets the maximum time (in milliseconds) that Playwright will wait for an action to complete before timing out. If an action, such as clicking a button or filling out a form, takes longer than this specified time to complete, Playwright will consider it a failure and throw a timeout error.      
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
+      name: "setup",
+      testMatch: /tests\/setup\/.setup\.ts/,
+    },
+
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'], storageState: STORAGE_STATE },
+      dependencies: ["setup"],
+      testIgnore: [/tests\/setup\/.*/]
+    },
+
+    /*
+     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+     */
 
     {
       name: 'firefox',
